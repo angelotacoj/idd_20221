1a957f742be24185e0018f33163f3938d0b94c28
angelotacoj <angelotacoj@gmail.com>
2022-06-19 14:38:25 -0500

    add: getStudentsAndAttorney

--- helper.js
+++ helper.js
@@ -30,9 +30,36 @@ async function runSelect(query) {
     }
 }
 
+async function runSelect(query) {
+    let conn
+
+    try {
+        conn = await oracledb.getConnection(dbConfig)
+        const result = await conn.execute(query)
+        return result
+    } catch (err) {
+        console.error('err 1 =', err)
+    } finally {
+        try {
+            if (conn) await conn.close()
+        } catch (err) {
+            console.error('err 2 =', err)
+        }
+    }
+}
+
 async function getAllWorkshops() {
-    const result = await runSelect('SELECT * FROM TALLER')
+    const workshops = await runSelect('SELECT * FROM TALLER')
     console.log('workshops =', workshops)
 }
 
-getAllWorkshops()
+async function getStudentsAndAttorney() {
+    const studentsAndAttorney = await runSelect(`SELECT UPPER(U.NOMBRES_NOMBRE || ' ' || U.APELLIDOP_NOMBRE || ' ' || U.APELLIDOM_NOMBRE) AS FULLNAME, A.DP_DATOSAPODERADO
+    FROM USUARIO U
+    INNER JOIN ALUMNO A 
+    ON U.DNI = COD_ALUMNO`)
+    console.log('studentsAndAttorney =', studentsAndAttorney)
+}
+
+// getAllWorkshops()
+getStudentsAndAttorney()
