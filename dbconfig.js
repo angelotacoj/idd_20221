649c52059c30264e3061cfc9738e68f60f6e6e55
jaimytaco <jaimy.taco@gmail.com>
2022-06-19 12:59:25 -0500

    feat: config TNS_ADMIN

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,12 @@
 module.exports = {
-    user          : "angelotacoj",
-    // password      : process.env.NODE_ORACLEDB_PASSWORD,
-    password: "20181849@aloe.ulima.edu.PE",
-    // connectString : "localhost/XEPDB1"
-    connectString : "adb.sa-saopaulo-1.oraclecloud.com:1522"
-  };
+  user: "angelotacoj",
+  // password      : process.env.NODE_ORACLEDB_PASSWORD,
+  password: "20181849@aloe.ulima.edu.PE",
+  // connectString : "localhost/XEPDB1"
+  // connectString : "adb.sa-saopaulo-1.oraclecloud.com:1522"
+  connectString: "db20220613222409_tp",
+  poolMin: 2,
+  poolMax: 2,
+  poolIncrement: 0,
+  events: false
+};
