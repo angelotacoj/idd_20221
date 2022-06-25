eee823fdf00d603e7e4fd08c283f23fd3679179e
angelotacoj <angelotacoj@gmail.com>
2022-06-25 12:38:01 -0500

    feat: add project_mpe

--- helper.mjs
+++ helper.mjs
@@ -92,7 +92,110 @@ async function updateWorkshop(){
 export async function getAllWorkshops() {
     const workshops = await runSelect('SELECT * FROM TALLER')
     console.log('workshops =', workshops)
-    return '<h1>Talleres</h1>'
+    return `
+            <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="style.css">
+            <title>Document</title>
+        </head>
+        <body>
+            <nav class="border-bottom">
+                <img src="media/139191135_3819700778053406_7422346157977545339_n.png" alt="" style="width: 60px; height: 60px;">
+                <div class="d-flex flex-column justify-content-center ms-3">
+                    <h5 >SISTEMA ESCOLAR</h5>
+                    <h6>Editar perfil</h6>
+                </div>
+                <a href="login.html" style="margin-left: auto; display: flex; padding: 10px; text-decoration: none;" >
+                    <button type="button" class="btn btn-info">Menu principal</button>
+                </a>
+            </nav>
+            <div class="container" id="borde">
+
+                <div class="row">
+                    <div class="col-9">
+
+                        <h3>Agrege el taller</h3>
+                        <table class="table table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th scope="col">Rubro</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Fecha</th>
+                                    <th scope="col">Hora</th>
+                                    <th scope="col">ID</th>
+
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+
+                                    <td>Rubro 1</td>
+                                    <td>Mark</td>
+                                    <td>22/06/2022</td>
+                                    <td>Smith</td>
+                                    <td>0020</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Rubro 1</td>
+                                    <td>Mark</td>
+                                    <td>22/06/2022</td>
+                                    <td>Smith</td>
+                                    <td>1230</td>
+                                </tr>
+                                <tr>
+                                    <td>Rubro 2</td>
+                                    <td>Mark</td>
+                                    <td>22/06/2022</td>
+                                    <td>Smith</td>
+                                    <td>4611</td>
+                                </tr>
+                                <tr>
+                                    <td>Rubro 3</td>
+                                    <td>Mark</td>
+                                    <td>22/06/2022</td>
+                                    <td>Smith</td>
+                                    <td>1615</td>
+                                </tr>
+                                <tr>
+                                    <td>20184512</td>
+                                    <td>Mark</td>
+                                    <td>22/06/2022</td>
+                                    <td>Smith</td>
+                                    <td>5to8787td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <div class="col-1">
+                        <div style="margin-top: 120px;">
+                            <button type="button" class="btn btn-primary">Editar</button>
+
+                            <button type="button" class="btn btn-success">Editar</button>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-success">AGREGAR TALLER</button>
+            </div>
+        </body>
+        </html>  
+    `
 }
 
 // Comando de prueba, Funcion que imprime al estudiante junto a su apoderado
